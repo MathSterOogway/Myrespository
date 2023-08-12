1ff86642aec19bdf5e113013c5fd848ed7fc7bce
John Eimann <johneimannculibrina@gmail.com>
2023-08-12 17:09:18 +0700

    Made a google search replica with google image

--- GoogleImageSearch.js
+++ GoogleImageSearch.js
@@ -1,26 +1,21 @@
 const clickableImage = document.getElementById('clickableImage');
 
 clickableImage.addEventListener('click', function() {
-  performGoogleSearch();
+  performGoogleImageSearch();
 });
-
 const searchInput = document.getElementById('searchInput');
 
 searchInput.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     event.preventDefault(); // Prevent form submission
-    performGoogleSearch();
+    performGoogleImageSearch();
   }
 });
 
-function performGoogleSearch() {
+function performGoogleImageSearch() {
   const query = searchInput.value;
   if (query.trim() !== '') {
-    const searchURL = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+    const searchURL = `https://www.google.com/search?tbm=isch&q=${encodeURIComponent(query)}`;
     window.location.href = searchURL;
   }
 }
-
-
-
-
